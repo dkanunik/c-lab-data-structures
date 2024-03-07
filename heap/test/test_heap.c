1638a76bdde26ea0163c408b4db956b8ea8f6045
d.kanunik <6085168+dkanunik@users.noreply.github.com>
2024-03-07 22:03:39 +0200

    add test data generator

--- heap/test/test_heap.c
+++ heap/test/test_heap.c
@@ -1,18 +1,96 @@
 #include <string.h>
+#include <stdlib.h>
+#include <time.h>
 #include "heap.h"
 #include "unity.h"
 
 Heap *heap = NULL;
-int MAX_HEAP_SIZE = 20;
+int MAX_DATA_SIZE = 100;
+int MAX_HEAP_SIZE = 100;
+int LAST_NUMBER = 0;
+char NUM_SEPARATOR = ' ';
+
+int generate_test_data(char *file_name, int count, int min, int max) {
+    srand(time(NULL));
+
+    FILE *file = fopen(file_name, "w");
+    if (file == NULL) {
+        fprintf(stderr, "Error opening file.\n");
+        return 1;
+    }
+
+    for (int i = 0; i < count; i++) {
+        int random_number = rand() % (max - min + 1) + min;
+        fprintf(file, "%d%c", random_number, NUM_SEPARATOR);
+    }
+
+    fprintf(file, "%d", LAST_NUMBER);
+
+    fclose(file);
+
+    printf("Random numbers saved to file '%s'.\n", file_name);
+
+    return 0;
+}
+
+int *get_test_data(const char *file_name) {
+    FILE *file = fopen(file_name, "r");
+    if (file == NULL) {
+        perror("Error opening file");
+        exit(EXIT_FAILURE);
+    }
+
+    int *data = (int *) malloc(MAX_DATA_SIZE
+                               * (sizeof(int) + sizeof(NUM_SEPARATOR))
+                               + sizeof(LAST_NUMBER));
+
+    int count = 0;
+    int num;
+    while (fscanf(file, "%d", &num) == 1) {
+        data[count++] = num;
+    }
+
+    fclose(file);
+
+    return data;
+
+}
+
+void heap_print(int *arr, int n, int i, int space) {
+    if (i >= n)
+        return;
+
+    space += 10;
+
+    heap_print(arr, n, 2 * i + 2, space);
+
+    printf("\n");
+    for (int j = 5; j < space; j++)
+        printf(" ");
+
+    printf("%d\n", arr[i]);
+
+    heap_print(arr, n, 2 * i + 1, space);
+}
+
+void heap_to_array() {
+    for (int i = 0; i < heap->size; i++) {
+        printf("%d ", heap->array[i]);
+    }
+    printf("\n");
+}
 
 void setUp() {
     heap = heap_create(MAX_HEAP_SIZE);
 
-    int test_data[] = {9, 2, 6,12, 7,1,11,5,15,9,19,17, 4,8};
-    int num_elements = sizeof(test_data) / sizeof(test_data[0]);
-    for (int i = 0; i < num_elements; i++) {
-        heap_insert(heap, test_data[i]);
+    //generate_test_data("test1.txt", 50, 1, 100);
+
+    int *data = get_test_data("test2.txt");
+
+    while (*data != 0) {
+        heap_insert(heap, *(data++));
     }
+
 }
 
 void tearDown() {
@@ -20,10 +98,7 @@ void tearDown() {
 }
 
 void test_heap_create(void) {
-    for (int i = 0; i < heap->size; i++) {
-        printf("%d ", heap->array[i]);
-    }
-    printf("\n");
+    heap_print(heap->array, heap->size, 0, 0);
 }
 
 int main(void) {
